4757c79c3a09bd4eb6f0092ed40eb6ba22b4c6fe
Kris Ezra <kris.ezra@gmail.com>
2024-10-24 12:47:48 -0400

    [docs] Things you may need to install on ubuntu

---
+++ install_deps_ubuntu.sh
@@ -0,0 +1,12 @@
+# For st
+sudo apt install libx11-dev
+sudo apt install libxft-dev -y
+sudo apt install fonts-hack-ttf
+
+# For zsh
+sudo apt install ripgrep
+sudo apt install bat -y
+
+# For nvim
+sudo apt install python3-pip
+python3 -m pip install pynvim
