83e76f5da6de432b439cba0b0bf5f13aa1338b04
Kris Ezra <kris.ezra@gmail.com>
2024-10-24 12:54:32 -0400

    Updates to install guide for ubuntu

--- install_deps_ubuntu.sh
+++ install_deps_ubuntu.sh
@@ -3,6 +3,11 @@ sudo apt install libx11-dev
 sudo apt install libxft-dev -y
 sudo apt install fonts-hack-ttf
 
+# For dwm
+sudo apt install libxinerama-dev
+libx11-xcb-dev
+libxcb-res0-dev
+
 # For zsh
 sudo apt install ripgrep
 sudo apt install bat -y
